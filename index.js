b3d6827734d2af1df9c131507d1983c2c2f8dc28
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-05-08 01:29:17 +0530

    Auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const ShortUrl  = require("./models/urlModel")
+const bcrypt = require('bcrypt')
 
 const app =express();
 app.set("view engine",'ejs')
@@ -9,9 +10,35 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+const users = []
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
     res.render('index', { shortUrls: shortUrls })
+  })
+  app.get('/login', (req, res) => {
+    res.render('login.ejs')
+  })
+  // app.post('/login' {
+  //   successRedirect: '/',
+  //   failureRedirect: '/login',
+  //   failureFlash: true
+  // }))
+  app.get('/register', (req, res) => {
+    res.render('register.ejs')
+  })
+  app.post('/register',  async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
   })
   
   app.post('/shortUrls', async (req, res) => {
